ac649f541d5aacaec7006430c1981e3f1c2120c2
Markus <markus2740@gmail.com>
2018-09-26 15:36:37 +0200

    added missing files

--- Scripts/New Test Case/Script1537890203637.groovy
+++ Scripts/New Test Case/Script1537890203637.groovy
@@ -17,23 +17,51 @@ WebUI.openBrowser('')
 
 WebUI.navigateToUrl('http://web.retriever-info.com/services/archive.html?')
 
-WebUI.click(findTestObject('test_folder_2/Page_Sk i ATEKST/a_Utvidet sk'))
+WebUI.click(findTestObject('Object Repository/test_folder_2/Page_Sk i ATEKST/a_Utvidet sk'))
 
-WebUI.setText(findTestObject('test_folder_2/Page_Sk i ATEKST/textarea_alle ordene_allOfTheW'), 'hydro')
+WebUI.setText(findTestObject('Object Repository/test_folder_2/Page_Sk i ATEKST/textarea_alle ordene_allOfTheW'), 'hydro')
 
-WebUI.selectOptionByValue(findTestObject('test_folder_2/Page_Sk i ATEKST/select_20 treff per side50 tre'), '100', true)
+WebUI.selectOptionByValue(findTestObject('Object Repository/test_folder_2/Page_Sk i ATEKST/select_20 treff per side50 tre'), 
+    '100', true)
 
-WebUI.selectOptionByValue(findTestObject('test_folder_2/Page_Sk i ATEKST/select_hele artikkelenbare ove'), 'searchInDocumentTeaserOnly', 
-    true)
+WebUI.selectOptionByValue(findTestObject('Object Repository/test_folder_2/Page_Sk i ATEKST/select_hele artikkelenbare ove'), 
+    'searchInDocumentTeaserOnly', true)
 
-WebUI.click(findTestObject('test_folder_2/Page_Sk i ATEKST/input_Endre sk_searchButtonBot'))
+WebUI.click(findTestObject('Object Repository/test_folder_2/Page_Sk i ATEKST/input_Endre sk_searchButtonBot'))
 
-for (def index : (0..5)) {
+for (def index : (0..1)) {
+    WebUI.click(findTestObject('Object Repository/test_folder_3/Page_Retriever Research hydro Sourc/div_0 (1)'))
+
+    WebUI.click(findTestObject('Object Repository/test_folder_3/Page_Retriever Research hydro Sourc/a_Alle p siden (1)'))
+
+    WebUI.click(findTestObject('Object Repository/test_folder_3/Page_Retriever Research hydro Sourc/a_Neste side (1)'))
 }
 
-WebUI.click(findTestObject('Object Repository/test_folder_2/Page_Retriever Research hydro Sourc/div_0'))
+WebUI.click(findTestObject('test_folder_3/Page_Retriever Research hydro Sourc/i_Ingen_icon reticon-download (1)'))
 
-WebUI.click(findTestObject('Object Repository/test_folder_2/Page_Retriever Research hydro Sourc/a_Alle p siden'))
+WebUI.waitForElementVisible(findTestObject('test_folder_3/Page_Retriever Research hydro Sourc/iframe_concat(id(  fancybox-fr'), 
+    2)
 
-WebUI.click(findTestObject('Object Repository/test_folder_2/Page_Retriever Research hydro Sourc/a_Neste side'))
+WebUI.click(findTestObject('Object Repository/test_folder_3/Page_Retriever Research hydro Sourc/span_Ren tekst'))
+
+WebUI.click(findTestObject('Object Repository/test_folder_3/Page_Retriever Research hydro Sourc/a_Vis innstillinger (1)'))
+
+WebUI.click(findTestObject('Object Repository/test_folder_3/Page_Retriever Research hydro Sourc/a_Innholdsfortegnelse (1)'))
+
+WebUI.selectOptionByValue(findTestObject('Object Repository/test_folder_3/Page_Retriever Research hydro Sourc/select_inkludr innholdsfortegn (1)'), 
+    'NO_TOC', true)
+
+WebUI.click(findTestObject('Object Repository/test_folder_3/Page_Retriever Research hydro Sourc/input_Tilbakestill_saveArticle (1)'))
+
+WebUI.waitForAlert(1)
+
+WebUI.click(findTestObject('Object Repository/test_folder_3/Page_Retriever Research hydro Sourc/a_supportnorgeretriever.no_fan (1)'))
+
+WebUI.waitForAlert(4)
+
+WebUI.click(findTestObject('test_folder_3/Page_Retriever Research hydro Sourc/div_0 (1)'))
+
+WebUI.click(findTestObject('Object Repository/test_folder_3/Page_Retriever Research hydro Sourc/a_Ingen (1)'))
+
+WebUI.click(findTestObject('Object Repository/test_folder_3/Page_Retriever Research hydro Sourc/a_Neste side (1)'))
 
